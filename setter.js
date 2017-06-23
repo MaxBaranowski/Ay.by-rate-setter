5cf4b23cd811265bbfb2ad14bb4f5a2a59e1125f
Max Baranowski <MaxBaranowski@users.noreply.github.com>
2017-06-23 10:58:42 +0300

    Create setter.js

---
+++ setter.js
@@ -0,0 +1,15 @@
+/*
+необходимо первести экран в мобильный
+после того возможно увеличивать ставки кнопками
+*/
+var ratePlus = document.getElementsByClassName("lot-input-increment")[0];
+var rateMinus = document.getElementsByClassName("lot-input-increment")[1];
+/*
+минус/плюс к размеру ставки
+rateMinus.click();
+ratePlus.click();
+
+setRate - кнопка принять ставку
+setRate.click();
+*/
+var setRate = document.getElementsByClassName("i-button i-button_large i-button_primary show-popup")[0];
